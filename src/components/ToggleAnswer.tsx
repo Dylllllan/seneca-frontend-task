4bb32c67e16837b78804a1af3d601481e615a0e8
Dylan <31136360+Dylllllan@users.noreply.github.com>
2024-10-29 23:56:19 +0000

    Implement selecting answers
    - Selected answers state in the question component
    - Animate padding in the slider based on chosen answer
    - Lock the question when all answers are correct

--- src/components/ToggleAnswer.tsx
+++ src/components/ToggleAnswer.tsx
@@ -2,19 +2,27 @@ import "../styles/ToggleAnswer.scss";
 
 type Props = {
     options: string[];
+    selectedOption: string;
+    onSelect: (option: string) => void;
 };
 
-function ToggleAnswer({ options }: Props) {
+function ToggleAnswer({ options, selectedOption, onSelect }: Props) {
+    const selectedIndex = options.indexOf(selectedOption);
+
+    // Calculate the flexbox padding for the slider
+    const leftSliderPadding = selectedIndex; // i.e. first option has no padding
+    const rightSliderPadding = options.length - selectedIndex - 1; // i.e. last option has no padding
+
     return (
         <div className="ToggleAnswer">
             <div className="slider">
-                <div className="sliderPadding" style={{ flexGrow: 0 }} />
-                <div className="sliderButton" />
-                <div className="sliderPadding" style={{ flexGrow: 1 }} />
+                <div className="sliderPadding" style={{ flexGrow: leftSliderPadding }} />
+                <div className="sliderCell" />
+                <div className="sliderPadding" style={{ flexGrow: rightSliderPadding }} />
             </div>
             <div className="options">
                 {options.map((option, index) => (
-                    <div key={index} className="option">
+                    <div key={index} className="option" onClick={() => onSelect(option)}>
                         {option}
                     </div>
                 ))}
--- src/components/ToggleQuestion.tsx
+++ src/components/ToggleQuestion.tsx
@@ -1,5 +1,6 @@
-import "../styles/ToggleQuestion.scss";
+import { useState } from "react";
 
+import "../styles/ToggleQuestion.scss";
 import ToggleAnswer from "./ToggleAnswer";
 
 type Props = {
@@ -11,6 +12,29 @@ type Props = {
 };
 
 function ToggleQuestion({ question, answers }: Props) {
+    // Initialise the selected answers as the first option for each answer
+    const [selectedAnswers, setSelectedAnswers] = useState<string[]>(answers.map(({ options }) => options[0]));
+
+    const score = selectedAnswers.reduce((score, selectedAnswer, index) => {
+        const correctAnswer = answers[index].correct;
+        return score + (selectedAnswer === correctAnswer ? 1 : 0);
+    }, 0);
+
+    const isCorrect = score === selectedAnswers.length;
+
+    const selectAnswer = (index: number, option: string) => {
+        if (isCorrect) {
+            // All answers are correct, so the question is locked
+            return;
+        }
+
+        setSelectedAnswers((currentAnswers) => {
+            return currentAnswers.map((currentAnswer, currentIndex) => {
+                return currentIndex === index ? option : currentAnswer;
+            });
+        });
+    };
+
     return (
         <div className="ToggleQuestion">
             <h2 className="question">{question}</h2>
@@ -19,10 +43,12 @@ function ToggleQuestion({ question, answers }: Props) {
                     <ToggleAnswer
                         key={index}
                         options={options}
+                        selectedOption={selectedAnswers[index]}
+                        onSelect={(option) => selectAnswer(index, option)}
                     />
                 ))}
             </div>
-            <h3 className="answerStatus">The answer is incorrect</h3>
+            <h3 className="answerStatus">The answer is {isCorrect ? "correct" : "incorrect"}</h3>
         </div>
     );
 }
